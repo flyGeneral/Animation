8b00e2aa58f27f5e1e7ca9ca92487fb05aa95f6f
flyGeneral <479736308@qq.com>
2016-12-31 16:10:20 +0800

    更新gradle配置，增加svg图片动画示例

--- app/src/main/java/com/person/chenjinghao/animation/SVGImageActivity.java
+++ app/src/main/java/com/person/chenjinghao/animation/SVGImageActivity.java
@@ -1,10 +1,14 @@
 package com.person.chenjinghao.animation;
 
+import android.graphics.drawable.Animatable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatDelegate;
+import android.view.View;
+import android.widget.ImageView;
 
-public class SVGImageActivity extends AppCompatActivity {
+public class SVGImageActivity extends AppCompatActivity implements View.OnClickListener {
 
     /**
      * 在Android 5.0以下的系统中，这是5.0以下系统兼容vectorDrawable遗留下来的坑
@@ -15,9 +19,34 @@ public class SVGImageActivity extends AppCompatActivity {
         AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
     }
 
+    private ImageView ivLeftRightArrow;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_svgimage);
+
+        initWidght();
+        initEvents();
+    }
+
+    private void initEvents() {
+        ivLeftRightArrow.setOnClickListener(this);
+    }
+
+    private void initWidght() {
+        ivLeftRightArrow = (ImageView) findViewById(R.id.iv_left_right_arrow);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.iv_left_right_arrow:
+                Drawable drawable = ivLeftRightArrow.getDrawable();
+                if (drawable instanceof Animatable){
+                    ((Animatable)drawable).start();
+                }
+                break;
+        }
     }
 }
